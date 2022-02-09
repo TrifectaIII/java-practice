4732c99e0f0f638d79bd420637343686c1e0341d
Dakota Madden-Fong <maddenfong@gmail.com>
2022-02-09 09:45:40 -0800

    added primitive data types

--- src/App.java
+++ src/App.java
@@ -1,7 +1,23 @@
 public class App {
+    private static final String STRING = "Hello, World!"; // variable size
+    private static final byte BYTE = 100; // 1 byte
+    private static final short SHORT = 30000; // 2 bytes
+    private static final int INTEGER = -2000000000; // 4 bytes
+    private static final long LONG = 9000000000000000000L; // 8 bytes
+    private static final float FLOAT = 5.75f; // 4 bytes
+    private static final double DOUBLE = 19.99d; // 8 bytes
+    private static final boolean BOOLEAN = true; // 1 bit
+    private static final char CHAR = 'A'; // 2 bytes
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World2!");
-        System.out.println("Hello, World3!");
+        System.out.println(STRING);
+        System.out.println(BYTE);
+        System.out.println(SHORT);
+        System.out.println(INTEGER);
+        System.out.println(LONG);
+        System.out.println(FLOAT);
+        System.out.println(DOUBLE);
+        System.out.println(BOOLEAN);
+        System.out.println(CHAR);
     }
 }
